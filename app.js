2d9f7a7cf6c66b450eb69895f29f438c397c7a5a
John Hannafin <john.hannafin@lit.ie>
2016-01-19 15:38:21 +0000

    Exploring objects in JS

--- app.js
+++ app.js
@@ -21,3 +21,64 @@ if (age == 21 ) {
 
 var o = new Object();
 var anotherObject = {};
+
+ /*
+AS Code:
+
+public class Person {
+	public var name:String;
+	public var age:int;
+	public var male:boolean;
+
+	function Person() {
+		// default to female
+		this.male = false;
+	}
+
+	public growOld():void {
+		this.age++;
+	}
+}
+
+// Somewhere else
+
+var p:Person = new Person();
+
+p.name = "John";
+p.age = 21;
+p.male = true;
+
+p.growOld();
+
+*/
+var person = {};
+person.name = "John";
+person.age = 21;
+person.male = true;
+person.growOld = function () {
+	this.age++;
+};
+
+var anotherPerson = {
+	name: "Mary",
+	age: 25,
+	male: false,
+	growOld: function() {
+		this.age++;
+	},
+	friends: ["Tom","Pat"]
+};
+
+for(var i=0;i<10;i++) {
+	console.log(anotherPerson.name + " is " + anotherPerson.age);
+	anotherPerson.growOld();
+}
+
+
+
+
+
+
+
+
+
