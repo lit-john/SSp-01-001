1c566ad8fa13f4022c948b255c3d9995995da90a
John Hannafin <john.hannafin@lit.ie>
2016-01-19 15:16:07 +0000

    Playing with variables in JS

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // Just printing to the console 
 console.log("Hello world");
+
+function add(a, b) {
+	var ans = a + b;
+	return ans;
+}
+
+var theAns;
+theAns = add(5, 10);
+console.log("5 + 10 is " + theAns);
+
+var name = "John"; //String
+var age = 21; // int
+var male = true; // boolean
+var myArray = new Array(); //Array
+
+if (age == 21 ) {
+	console.log("Your still young " + name);
+}
+
+var o = new Object();
+var anotherObject = {};
